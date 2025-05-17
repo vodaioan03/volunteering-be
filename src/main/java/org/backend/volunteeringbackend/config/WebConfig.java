265ab9c857ed6ad7bb496d4c07a74ba603b7841e
Voda Ioan <83557961+vodaioan03@users.noreply.github.com>
2025-05-18 00:07:04 +0300

    fix:app.prop

--- src/main/java/org/backend/volunteeringbackend/config/WebConfig.java
+++ src/main/java/org/backend/volunteeringbackend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 //.allowedOrigins("http://localhost:3000") // Allow all origins
-                .allowedOrigins("*") // Allows ALL origins
+                .allowedOriginPatterns("*") // Allows ALL origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true)
